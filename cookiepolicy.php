8ac5aa9875231e614aa62d5f14e21443401bee9a
Simon Cooper <cooper.sf@gmail.com>
2017-03-24 21:19:31 +0000

    Adding Cookie Policy

---
+++ cookiepolicy.php
@@ -0,0 +1,165 @@
+<?php
+// Connect to mySQL
+include ('includes/pdo_db_con.php');
+$conn = dbConnectpdo();
+$stmt = $conn->prepare("SELECT * FROM articles where article_id = :article_id ORDER BY article_id DESC");
+$stmt->bindParam(':article_id', $_GET['article_id'], PDO::PARAM_INT);
+$stmt->execute();
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--CSS Files-->
+    <link rel="stylesheet" href="css/uikit.min.css" />
+    <title>Gourmet Coffee London - Cookie Policy</title>
+  </head>
+  <body>
+<!--Nav code--><?php include ('includes/main_nav.php'); ?>
+<!--Main header img--><div class="uk-inline uk-width-1-1">
+        <img src="img/header_img_thin.jpg" alt="" class="uk-width-1-1">
+        <div class="uk-overlay-primary uk-position-cover"></div>
+        <div class="uk-overlay uk-position-center uk-light">
+        </div>
+      <!--End main header img--></div>
+<div class="uk-container"><!--Start of main wrapper-->
+  <div class="uk-child-width-expand@s" uk-grid><!--Start of grid-->
+
+    <div><!--Start left column-->
+        <div class="uk-card uk-card-default uk-card-body">
+          <article class="uk-article">
+
+                     <h1 class="uk-article-title">Cookie Policy</h1>
+                     <h3>What Are Cookies</h3>
+
+
+                     <p>As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break' certain elements of the sites functionality.</p>
+
+                     <p>For more general information on cookies see the Wikipedia article on HTTP Cookies...</p>
+                     <h3>How We Use Cookies</h3>
+
+
+                     <p>We use cookies for a variety of reasons detailed below. Unfortunately is most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.</p>
+
+                     <h3>Disabling Cookies</h3>
+
+
+                     <p>You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of the this site. Therefore it is recommended that you do not disable cookies.</p>
+
+                     <h3>The Cookies We Set</h3>
+
+                     		  <p>Currently we only set one cookie ourselves (1st Party Cookie) which is to store your decision on agreeing to accept cookies or not.</p>
+
+
+
+
+
+                     <h3>Third Party Cookies</h3>
+
+
+                     <p>In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.
+
+                     This site uses Google Analytics which is one of the most widespread and trusted analytics solution on the web for helping us to understand how you use the site and ways that we can improve your experience. These cookies may track things such as how long you spend on the site and the pages that you visit so we can continue to produce engaging content.
+
+                     For more information on Google Analytics cookies, see the official Google Analytics page.
+
+                     Third party analytics are used to track and measure usage of this site so that we can continue to produce engaging content. These cookies may track things such as how long you spend on the site or pages you visit which helps us to understand how we can improve the site for you.
+
+                     From time to time we test new features and make subtle changes to the way that the site is delivered. When we are still testing new features these cookies may be used to ensure that you receive a consistent experience whilst on the site whilst ensuring we understand which optimisations our users appreciate the most.
+
+                     As we sell products it's important for us to understand statistics about how many of the visitors to our site actually make a purchase and as such this is the kind of data that these cookies will track. This is important to you as it means that we can accurately make business predictions that allow us to monitor our advertising and product costs to ensure the best possible price.
+
+                     The Google AdSense service we use to serve advertising uses a DoubleClick cookie to serve more relevant ads across the web and limit the number of times that a given ad is shown to you.
+
+                     For more information on Google AdSense see the official Google AdSense privacy FAQ.
+
+                     We use adverts to offset the costs of running this site and provide funding for further development. The behavioural advertising cookies used by this site are designed to ensure that we provide you with the most relevant adverts where possible by anonymously tracking your interests and presenting similar things that may be of interest.
+
+                     In some cases we may provide you with custom content based on what you tell us about yourself either directly or indirectly by linking a social media account. These types of cookies simply allow us to provide you with content that we feel may be of interest to you.
+
+                     Several partners advertise on our behalf and affiliate tracking cookies simply allow us to see if our customers have come to the site through one of our partner sites so that we can credit them appropriately and where applicable allow our affiliate partners to provide any bonus that they may provide you for making a purchase.
+
+                     We also use social media buttons and/or plugins on this site that allow you to connect with your social network in various ways. For these to work the following social media sites including; Facebook and Twitter and will set cookies through our site which may be used to enhance your profile on their site or contribute to the data they hold for various purposes outlined in their respective privacy policies.</p>
+
+
+                     <h3>More Information</h3>
+
+
+                     <p>Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it does interact with one of the features you use on our site. However if you are still looking for more information then you can contact us through one of our preferred contact methods.<p>
+                     <p class="uk-article-meta">
+
+                     </p>
+
+
+            </article>
+
+
+        </div>
+    </div><!--End left column-->
+    <div class="uk-width-1-3"><!--Start right column-->
+        <div class="uk-card uk-card-default uk-card-body">Item</div>
+    </div><!--End right column-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </div><!--End of main grid-->
+</div><!--End of main wrapper-->
+
+<!--Start of footer-->
+<div class="uk-section uk-section-secondary uk-light">
+    <div class="uk-container">
+
+        <h3>Section Secondary</h3>
+
+        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </div>
+        </div>
+
+    </div>
+</div>
+  <!--JS Scripts-->
+  <script src="js/jquery.min.js"></script>
+<script src="js/uikit.min.js"></script>
+<script src="js/uikit-icons.min.js"></script>
+<!--GA Tracking-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-211576-10', 'auto');
+  ga('send', 'pageview');
+
+</script>
+  </body>
+  </html>
